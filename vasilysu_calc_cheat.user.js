28d8974531a4ea103bcfb008f7cf70037bb704c1
d!key <79040270950@ya.ru>
2021-08-19 08:07:19 +0300

    v1.2

--- vasilysu_calc_cheat.user.js
+++ vasilysu_calc_cheat.user.js
@@ -2,7 +2,7 @@
 // @name         vasilysu_calc_cheat
 // @namespace    http://github.com/
 // @homepage     http://dikey0ficial.github.io/vasilysu_calc_cheat
-// @version      1.1
+// @version      1.2
 // @description  Simple cheat script to skip unsolvable games on http://vasily.su/calculator-game/
 // @author       d!key
 // @match        http://vasily.su/calculator-game/level_random1.html
@@ -22,14 +22,12 @@
     button.title = "Cheat made by d!key";
     if (document.location.href == "http://vasily.su/calculator-game/level_random4.html") {
          button.onclick = function() {
-            firstnumber = answer;
-            number1 = 0;
+            firstnumber = answer - number1;
             firstButton();
         };
     } else {
         button.onclick = function() {
-            firstnumber = answer;
-            number2 = 0;
+            firstnumber = answer + number2;
             secondButton();
         };
     }
